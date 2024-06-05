4d3a169520461b4e138d23c7993b9601b85d3b87
musrex <leandro.n.lopez@gmail.com>
2024-06-04 21:53:45 -0400

    add base js to start on dropdown menu

---
+++ theme/static/js/scripts.js
@@ -0,0 +1,10 @@
+addEventListener("DOMContentLoaded", (event) => {
+    
+    let nav = getElementById("nav-bar");
+    let dropDown = getElementById("hamburger-menu");
+    
+    addEventListener(nav, (click) => {
+
+    }
+
+};
